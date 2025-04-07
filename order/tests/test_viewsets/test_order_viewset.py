cab36ef1a115832995363af550be062982e321e0
danielcruz <danielcscruz@gmail.com>
2025-04-07 20:52:12 -0300

    changed token auth to order

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -2,6 +2,8 @@ import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
+
 
 from django.urls import reverse
 
@@ -15,11 +17,17 @@ class TestOrderViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
+        
         self.category = CategoryFactory(title='technology')
         self.product = ProductFactory(title="mouse", price=100, category=[self.category])
         self.order = OrderFactory(product=[self.product])
 
     def test_order(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = self.client.get(
             reverse('order-list', kwargs={'version': 'v1'})
         )
@@ -33,6 +41,8 @@ class TestOrderViewSet(APITestCase):
         self.assertEqual(order_data['results'][0]['product'][0]['category'][0]['title'], self.category.title)
 
     def test_create_order(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         user = UserFactory()
         product = ProductFactory()
         data = json.dumps({
