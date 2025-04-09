b85d4d478c967de31ba21d5e9e43f48d8857eeac
danielcruz <danielcscruz@gmail.com>
2025-04-09 17:12:23 -0300

    added heroku allowed host

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -159,4 +159,4 @@ INTERNAL_IPS = [
 # DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://codan-bookstore-api-f48292c64a22.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://codan-bookstore-api-f48292c64a22.herokuapp.com', 'codan-bookstore-api-f48292c64a22.herokuapp.com']
